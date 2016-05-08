13c9724548c41fbd71da5611a759225b1544dc28
simonaco <simona.cotin@gmail.com>
2016-05-08 09:54:32 +0100

    Added socket.io dependecy in module loader

--- systemjs.config.js
+++ systemjs.config.js
@@ -10,7 +10,8 @@
     'app':                        'app', // 'dist',
     'rxjs':                       'node_modules/rxjs',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    '@angular':                   'node_modules/@angular'
+    '@angular':                   'node_modules/@angular',
+    'socket.io-client':           'node_modules/socket.io-client/socket.io.js'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
@@ -18,6 +19,7 @@
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { defaultExtension: 'js' },
+    'socket.io-client':           { defaultExtension: 'js' }
   };
 
   var packageNames = [
